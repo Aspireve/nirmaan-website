9a5274c86bc53faa0b5fd8f70fb197ef645d2ad0
Aman Prajapati <pjaman05671@gmail.com>
2024-03-28 20:25:48 +0530

    removed sponsor

--- app/Partners/page.jsx
+++ app/Partners/page.jsx
@@ -12,10 +12,10 @@ function page() {
 
         <div className=" text-white">
           <div className="text-5xl flex justify-center font-semibold items-center pb-10 pt-5 text-white">
-            <div className="bg-purple-500 w-20 h-3"></div>
+            <div className="bg-purple-500 w-14 h-3"></div>
             <div className="px-3">PARTNERS</div>
 
-            <div className="bg-purple-500 w-20 h-3"></div>
+            <div className="bg-purple-500 w-14 h-3"></div>
           </div>
           <div className="text-center px-16 mb-10 text-lg">
             We would like to thank our sponsors for their interest in our
@@ -219,33 +219,8 @@ function page() {
                   </a>
                 </div>
               </div>
-              <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
-                <div className="h-60 w-96">
-                  <img
-                    className="h-full w-full object-none transition-transform duration-500 group-hover:scale-125"
-                    src="arrowtex.png"
-                    alt=""
-                  />
-                </div>
-                <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent  group-hover:from-black/64 group-hover:via-black/60 group-hover:to-black/64"></div>
-                <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
-                  <div className="mb-3 text-lg text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-                    <div className="font-dmserif text-3xl font-bold text-white">
-                      ArrowTex
-                    </div>
-                    Provides carbon fiber
-                  </div>
-                  <a
-                    href="https://www.arrowtechnicaltextiles.in/"
-                    target="_blank"
-                  >
-                    <button className="rounded-full bg-[#25618B] py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">
-                      See More
-                    </button>
-                  </a>
-                </div>
-              </div>
-              <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 md:col-start-2">
+              
+              <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 ">
                 <div className="h-60 w-96">
                   <img
                     className="h-full w-full object-none transition-transform duration-500 group-hover:scale-125"
